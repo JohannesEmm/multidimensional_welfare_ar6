5e5c0d12e12a8bb3546a9032e6ecdb4bde8c9455
UlrikeKornek <78417013+UlrikeKornek@users.noreply.github.com>
2023-07-19 16:48:58 +0200

    Added selected-weights column
    Selected weights with low, medium and high GDP are now in the 'selected_weights' column. The weights column shows the unique weights identifier. I therefore removed the weights-dataframe.

--- main.R
+++ main.R
@@ -6,18 +6,17 @@ rm(list = ls())
 ### if welfares already exist, just load Rdata file
 ### if welfares exist, indicator.Rdata and weights.Rdata is assumed to exist as well, so this will also load
 
+library(ggplot2)
+library(dplyr)	
+library(stringr)
+library(tidyverse)
+library(imputeTS)
+library(data.table)
+
+
+
 if(!file.exists("welfares.Rdata"))
   {
-  
-
-  
-  library(ggplot2)
-  library(dplyr)	
-  library(stringr)
-  library(tidyverse)
-  library(imputeTS)
-  library(data.table)
-  
   ####################################################################################################    
   #########################        load functions       ##############################################
   ####################################################################################################    
@@ -34,8 +33,6 @@ if(!file.exists("welfares.Rdata"))
   
   file_name='ar6_data.Rdata';
   df=load(file_name);
-  
-  
   
   ####################################################################################################    
   #########################   specify column names that uniquely determine scenario   ################
@@ -54,7 +51,7 @@ if(!file.exists("welfares.Rdata"))
   print(variables)
   if (!is_empty(setdiff("Population",variables))){abort("Error: scenario data does not contain population")}
   
-  #specify which are evaluated with the log (these are usually consumption and GDP)
+  #specify which variables are evaluated with the log for the indicator value (these are usually consumption and GDP)
   variables_log=c("Consumption","GDP|PPP"); 
   if (!is_empty(setdiff(variables_log,variables))){abort("Error: list of log-variables not contained in variables")}
   
@@ -62,22 +59,19 @@ if(!file.exists("welfares.Rdata"))
   variables_bad=c("Emissions|Sulfur","Emissions|NOx","Emissions|CO2","AR6 climate diagnostics|Surface Temperature (GSAT)|MAGICCv7.5.3|50.0th Percentile");
   if (!is_empty(setdiff(variables_bad,variables))){abort("Error: list of bad-variables not contained in variables")}
   
-  #specify variables out of list with regional inequality assessment
-  #NOT IMPLEMENTED variables_regional=c();
-  
   #specify variables that have to be on a per-capita basis from list above (internal computation):
   variables_pop=c("Emissions|Sulfur","Emissions|NOx","Consumption","Emissions|CO2","GDP|PPP", "Final Energy|Electricity","Food Energy Supply");
   if (!is_empty(setdiff(variables_pop,variables))){print("Error: list of per-capita-variables not contained in variables")}
   
   # specify minimum and maximum for all variables 
   # for per-capita variables these should already be in per-capita terms
-  # for forest land cover, switching to share in land
+  # for forest land cover, switching to share of forest in land
   # if no maximum/minimum can be a priori specified, specify "NA" and the maximum/minimum across all models scenarios will be taken
   
   #ensure order of the following list is the same as in (variables_min/max)
   # finding extrema is documented in excel sheet AR6_min_max_updates
   # extrema from the data set are calculated in CheckOutliers.R file
-  variables_min=c(0, 0.402, -26 , 0.000493, 0.000115, 0.0009,1.62, NA, 0        , NA, 0.00279, 1827);
+  variables_min=c(0, 0.402, -26 , 0.000493, 0.000115, 0.0009,1.62, NA, 0   , NA, 0.00279, 1827);
   names(variables_min)=variables
   variables_max=c(NA, 161 , 35.1, 0.059   , 0.0575  , 0.254 , 180, NA, 0.61, NA, 0.00688, 4505);	
   names(variables_max)=variables
@@ -89,9 +83,6 @@ if(!file.exists("welfares.Rdata"))
   ####################################################################################################    
   ##########################     normative parameters     ############################################
   ####################################################################################################    
-  
-  # inequality aversion:
-  #NOT IMPLEMENTED epsilon=c(0,1,1.5);
   
   #substitutability between dimensions of welfare metric
   rho=c(0,1,5);
@@ -118,34 +109,28 @@ if(!file.exists("welfares.Rdata"))
   welfares=compute_welfare_df(indicators, rho, weights)
 
   ####################################################################################################    
-  ###############################     some cosmetics before saving dataframes#############################
+  ###############################     some cosmetics before saving dataframes   ######################
   ####################################################################################################    
   
-    
+  # reduce welfares-dataframe to those of SSP2 and where vetting did not fail, drop C8 scenario  
   welfares <- welfares %>%
     filter(Category!="C8")%>%
+    filter(Ssp_family==2)%>%
     dplyr::filter(Category!="failed-vetting")
   
-  # add column that identifies the scenario with (NOT IMPLEMENTED region and) time
-  welfares$identifier <- paste(welfares$model, welfares$scenario, sep="XXX")
+  # add column that identifies the scenario with time
+  welfares$identifier <- paste(welfares$model, welfares$scenario, welfares$year, sep="XXX")
   
   # add column that identifies weights
   welfares = welfares %>% unite(col="weights",c(14:25), sep = ", ", remove = FALSE, na.rm = FALSE)
   
-  #save output
+  # add column that identifies the three selected sets of weights with low, med and high GDP
+  welfares = welfares %>%
+    mutate(selected_weights= if_else(weights %in% c("0.01, 0, 0, 0.005, 0.005, 0.01, 1, 0, 0.01, 0, 0.01, 0", "0.01, 0, 0, 0.005, 0.005, 0.01, 0.01, 0, 0.01, 0, 0.01, 0", "1, 0, 0, 0.5, 0.5, 1, 0.01, 0, 1, 0, 1, 0"), weights, NA))
+  
+  # save output
   save(welfares, file = "welfares.Rdata") 
-  
-  # save selected weights with low, equal, and high gdp as extra dataframe:
-  weight1=c(1,0,0,0.5,0.5,1,100,0,1,0,1,0)
-  names(weight1)=variables
-  weight2=c(1,0,0,0.5,0.5,1,1  ,0,1,0,1,0)
-  names(weight2)=variables
-  weight3=c(1,0,0,0.5,0.5,1,0.01,0,1,0,1,0)
-  names(weight3)=variables
-  #save these selected weights 
-  weights=list(a=weight1, b=weight2, c=weight3)
-  save(weights, file = "weights.Rdata") 
-  
+ 
   
   ###############################rename variables#####################################################
   
@@ -170,7 +155,7 @@ if(!file.exists("welfares.Rdata"))
 }else{
   load("indicators.Rdata")
   load("welfares.Rdata")
-  load("weights.Rdata")
+  load("ar6_data.Rdata")
 }
 
 
