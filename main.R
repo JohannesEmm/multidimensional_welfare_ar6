a3a6b51275447e9b366f8b41972322f40dd15ed2
UlrikeKornek <78417013+UlrikeKornek@users.noreply.github.com>
2023-07-26 15:38:43 +0200

    Some updates on plots

--- main.R
+++ main.R
@@ -6,12 +6,12 @@ rm(list = ls())
 ### if welfares already exist, just load Rdata file
 ### if welfares exist, indicator.Rdata and weights.Rdata is assumed to exist as well, so this will also load
 
-library(ggplot2)
-library(dplyr)	
-library(stringr)
 library(tidyverse)
+library(ggpubr)
 library(imputeTS)
 library(data.table)
+library(RColorBrewer)
+library(latex2exp)
 
 
 
